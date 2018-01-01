6aab3daf131a551dcb3fb1d750d1f8b1a8b959d8
Levi Roth <levimroth@gmail.com>
2018-01-01 11:33:33 -0500

    Pass longident directly into expander

--- src/ppx_constr_test.ml
+++ src/ppx_constr_test.ml
@@ -2,17 +2,17 @@ open Ppx_core
 
 let name = "constr_test"
 
-let expand ~loc ~path:_ (constructor : string) constructor_argument =
+let expand ~loc ~path:_ constructor constructor_argument =
   let (module A) = Ast_builder.make loc in
   let pattern_argument = Option.map constructor_argument ~f:(fun _ -> A.ppat_any) in
-  let pattern = A.(ppat_construct {txt = lident constructor; loc} pattern_argument) in
+  let pattern = A.(ppat_construct (Located.mk constructor) pattern_argument) in
   [%expr function | [%p pattern] -> true | _ -> false]
 
 let ext =
   Extension.declare
     name
     Extension.Context.expression
-    Ast_pattern.(single_expr_payload (pexp_construct (lident __) __))
+    Ast_pattern.(single_expr_payload (pexp_construct __ __))
     expand
 
 let () = Ppx_driver.register_transformation name ~extensions:[ext]
